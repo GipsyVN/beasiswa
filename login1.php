79f08e1f287872986f4bec28e9fe6d1905f66df4
GipsyVN <hahaja1029@gmail.com>
2023-12-15 17:34:47 +0700

    Hehe

--- login1.php
+++ login1.php
@@ -32,7 +32,6 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="assets/css/style-login.css">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <title>Login</title>
 </head>
