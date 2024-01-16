2c382e016f84e681c28be4aeb5aa7bcf7b6d35b7
GipsyVN <hahaja1029@gmail.com>
2024-01-16 09:46:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ require "config.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Sistem Pendaftaran Mahasiswa</title>
+    <title>Sistem Pendaftaran Beasiswa</title>
 
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/datatables.min.css">
